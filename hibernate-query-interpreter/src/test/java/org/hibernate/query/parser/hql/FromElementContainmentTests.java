9153cd647520f99ed0b9334985d74ea752b1c969
Steve Ebersole <steve@hibernate.org>
2015-08-25 22:14:28 -0500

    file headers

--- hibernate-query-interpreter/src/test/java/org/hibernate/query/parser/hql/FromElementContainmentTests.java
+++ hibernate-query-interpreter/src/test/java/org/hibernate/query/parser/hql/FromElementContainmentTests.java
@@ -1,3 +1,9 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: Apache License, Version 2.0
+ * See the LICENSE file in the root directory or visit http://www.apache.org/licenses/LICENSE-2.0
+ */
 package org.hibernate.query.parser.hql;
 
 import org.hibernate.query.parser.SemanticQueryInterpreter;

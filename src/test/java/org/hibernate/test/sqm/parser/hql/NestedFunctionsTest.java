751b36428a7f6a0d88f261e328acc1ef96d17b57
Steve Ebersole <steve@hibernate.org>
2016-07-14 09:22:52 -0500

    explicit nested function call test for Guilaume

---
+++ src/test/java/org/hibernate/test/sqm/parser/hql/NestedFunctionsTest.java
@@ -0,0 +1,96 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: Apache License, Version 2.0
+ * See the LICENSE file in the root directory or visit http://www.apache.org/licenses/LICENSE-2.0
+ */
+package org.hibernate.test.sqm.parser.hql;
+
+import org.hibernate.sqm.domain.DomainMetamodel;
+import org.hibernate.sqm.domain.SingularAttribute;
+import org.hibernate.sqm.query.SelectStatement;
+import org.hibernate.sqm.query.expression.function.ConcatFunctionSqmExpression;
+import org.hibernate.sqm.query.expression.function.SubstringFunctionSqmExpression;
+import org.hibernate.sqm.query.select.Selection;
+
+import org.hibernate.test.sqm.ConsumerContextImpl;
+import org.hibernate.test.sqm.domain.EntityTypeImpl;
+import org.hibernate.test.sqm.domain.ExplicitDomainMetamodel;
+import org.hibernate.test.sqm.domain.StandardBasicTypeDescriptors;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hibernate.sqm.SemanticQueryInterpreter.interpret;
+
+/**
+ * @author Steve Ebersole
+ */
+public class NestedFunctionsTest {
+	final ConsumerContextImpl consumerContext = new ConsumerContextImpl( buildMetamodel() );
+
+	@Test
+	public void testSubstrInsideConcat() {
+		final SelectStatement statement = (SelectStatement) interpret( "select concat('111', substring('222222', 1, 3)) from Entity", consumerContext );
+		assertThat( statement.getQuerySpec().getSelectClause().getSelections().size(), is(1) );
+		final Selection selection = statement.getQuerySpec().getSelectClause().getSelections().get( 0 );
+		assertThat( selection.getExpression(), instanceOf( ConcatFunctionSqmExpression.class ) );
+		final ConcatFunctionSqmExpression concatFunction = (ConcatFunctionSqmExpression) selection.getExpression();
+		assertThat( concatFunction.getExpressions().size(), is(2) );
+		// check that the second expression/argument is the substr function
+		assertThat( concatFunction.getExpressions().get( 1 ), instanceOf( SubstringFunctionSqmExpression.class ) );
+	}
+
+	private DomainMetamodel buildMetamodel() {
+		ExplicitDomainMetamodel metamodel = new ExplicitDomainMetamodel();
+
+		EntityTypeImpl entityType = metamodel.makeEntityType( "com.acme.Entity" );
+		entityType.makeSingularAttribute(
+				"basic",
+				SingularAttribute.Classification.BASIC,
+				StandardBasicTypeDescriptors.INSTANCE.LONG
+		);
+		entityType.makeSingularAttribute(
+				"basic1",
+				SingularAttribute.Classification.BASIC,
+				StandardBasicTypeDescriptors.INSTANCE.LONG
+		);
+		entityType.makeSingularAttribute(
+				"basic2",
+				SingularAttribute.Classification.BASIC,
+				StandardBasicTypeDescriptors.INSTANCE.STRING
+		);
+		entityType.makeSingularAttribute(
+				"basic3",
+				SingularAttribute.Classification.BASIC,
+				StandardBasicTypeDescriptors.INSTANCE.STRING
+		);
+		entityType.makeSingularAttribute(
+				"basic4",
+				SingularAttribute.Classification.BASIC,
+				StandardBasicTypeDescriptors.INSTANCE.STRING
+		);
+		entityType.makeSingularAttribute(
+				"from",
+				SingularAttribute.Classification.BASIC,
+				StandardBasicTypeDescriptors.INSTANCE.STRING
+		);
+
+		EntityTypeImpl legType = metamodel.makeEntityType( "com.acme.Leg" );
+
+		EntityTypeImpl tripType = metamodel.makeEntityType( "com.acme.Trip" );
+		tripType.makeMapAttribute(
+				"mapLegs",
+				StandardBasicTypeDescriptors.INSTANCE.STRING,
+				legType
+		);
+		tripType.makeListAttribute(
+				"collectionLegs",
+				StandardBasicTypeDescriptors.INSTANCE.INTEGER,
+				legType
+		);
+
+		return metamodel;
+	}
+}
